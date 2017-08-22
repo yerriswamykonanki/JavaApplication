fa15aa28095a9e45ab66c84848898dd4ae2e67af
yerriswamykonanki <yerriswamykonanki@gmail.com>
2017-08-22 14:53:53 +0530

    commened the code

--- src/main/java/WelcomeController.java
+++ src/main/java/WelcomeController.java
@@ -1,13 +1,13 @@
 package com.mkyong.controller;
  
-import org.slf4j.Logger;
+/*import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+*/
 @Controller
 public class WelcomeController {
  
